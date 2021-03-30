aa3bb97bbf91c75a4f28d53141a70581ab87e5c3
mariusmoroiu98 <mariusmoroiu98@MVP.SP>
2021-03-30 08:34:30 +0000

    Adaugare server local + SharePoint Extension

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,54 @@
-var http=require('http')
+const { response } = require('express');
+var http=require('http');
+//var router = express.Router();
+var request = require('request');
 var express = require('express');
 var app = express();
+var cod;
+
+app.use(express.json());
+
+
 
 // on the request to root (localhost:8080/)
 app.get('/', function (req, res) {
+    
     res.send('<b>My</b> first express http server');
+    cod = req.query.code;
+    console.log(req.query);
+    console.log('############');
+
+    get_token_function(cod);
+
+});
+
+get_token_function = (cod) => {
+  request.post(
+    'https://msign-test.transsped.ro/csc/v0/oauth2/token',
+    {
+      json: {
+              grant_type: "authorization_code",
+              code: cod,
+              client_id: "msdiverse",
+              client_secret: "8KKhHnjKdYmAakc8",
+              redirect_uri: "http://localhost:8080/"
+      },
+    },
+    (error, res, body) => {
+      if (error) {
+        console.error(error)
+        return
+      }
+      console.log(res.statusCode)
+      console.log(res)
+    }
+  )
+};
+
+// start the server in the port 8080 !
+app.listen(8080, function () {
+    console.log('App listening on port 8080.');
+    console.log(' ------------- ');
 });
 
 // On localhost:8080/welcome
@@ -17,7 +61,3 @@ app.use(function(req, res, next) {
     res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
 });
 
-// start the server in the port 8080 !
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080.');
-});
